22e761b96bb82220498b6b44b529f7f62da8d1d4
Konstantin Leontyev <K.A.Leontyev@gmail.com>
2025-05-24 20:33:32 +0400

    refactor(app-header): fix comment

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -12,7 +12,7 @@ export const AppHeader = (): JSX.Element => {
     <header className={styles.header}>
       <nav className={`${styles.menu} p-4`}>
         <div className={styles.menu_part_left}>
-          {/*пока тут должны быть ссылки, а не например кнопки или абзацы*/}
+          {/* Тут должны быть ссылки, а не например кнопки или абзацы */}
           <a href='/' className={`${styles.link} ${styles.link_active}`}>
             <BurgerIcon type='primary' />
             <p className='text text_type_main-default ml-2'>Конструктор</p>
