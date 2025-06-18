f2ea722cbc808229cde7f00ae87273507c2b526a
Konstantin Leontyev <K.A.Leontyev@gmail.com>
2025-06-18 18:29:33 +0400

    feat(vitest): change vitest-setup file resolution to .ts

--- vite.config.ts
+++ vite.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./vitest-setup.js'],
+    setupFiles: ['./vitest-setup.ts'],
   },
   server: {
     open: true,
